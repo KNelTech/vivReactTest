69c1c40409b3d176ca24cacbc40d322dcd58ebdc
KNelTech <k.nelsontech@gmail.com>
2024-10-12 13:23:55 -0500

    Import / Export clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react';
-
+import {ItemList} from "./ItemList"
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ItemList />
     </div>
   );
 };
--- src/ItemList.tsx
+++ src/ItemList.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
+
 
 interface Item {
   userId: number;
@@ -7,13 +8,17 @@ interface Item {
   body: string;
 }
 
-const ItemList: FC = () => {
+export const ItemList: FC = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [selectedItem, setSelectedItem] = useState<Item | null>(null);
 
+
+
+
   useEffect(() => {
+
     const fetchData = async () => {
       try {
         const response = await fetch(
@@ -59,6 +64,3 @@ const ItemList: FC = () => {
     </div>
   );
 };
-
-
-export default ItemList;
